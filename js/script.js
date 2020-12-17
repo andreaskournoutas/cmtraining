5eded830023df2e78437d21c3f30c57a988121f7
Andreas Kournoutas <andreaskournoutas@gmail.com>
2020-12-17 15:37:21 +0200

    OneSignal testing

--- js/script.js
+++ js/script.js
@@ -360,6 +360,23 @@ function initializeOneSignal() {
         safari_web_id: 'web.onesignal.auto.21fd847c-14e1-48c8-a072-78170e2e9023',
         notifyButton: {
             enable: false,
+        },
+        promptOptions: {
+            customlink: {
+                enabled: true, /* Required to use the Custom Link */
+                style: "button", /* Has value of 'button' or 'link' */
+                size: "medium", /* One of 'small', 'medium', or 'large' */
+                color: {
+                    button: '#E12D30', /* Color of the button background if style = "button" */
+                    text: '#FFFFFF', /* Color of the prompt's text */
+                },
+                text: {
+                    subscribe: "Subscribe to push notifications", /* Prompt's text when not subscribed */
+                    unsubscribe: "Unsubscribe from push notifications", /* Prompt's text when subscribed */
+                    explanation: "Get updates from all sorts of things that matter to you", /* Optional text appearing before the prompt button */
+                },
+                unsubscribeEnabled: true, /* Controls whether the prompt is visible after subscription */
+            }
         }
     };
     OneSignal.push(function() {
